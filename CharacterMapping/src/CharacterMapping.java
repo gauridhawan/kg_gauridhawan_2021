e0d9ea2b061d3edbd863ecf18c6d2e4b777185ca
gauridhawan <dhawan.gauri@gmail.com>
2020-03-16 01:26:20 -0400

    added driver code and main function for identifying end to end mapping

---
+++ CharacterMapping/src/CharacterMapping.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class CharacterMapping {
+
+    public static void main(String[] args){
+
+        String s1 = args[0];
+        String s2 = args[1];
+
+        System.out.println(hasMapping(s1,s2));
+
+
+    }
+
+    public static boolean hasMapping(String s1, String s2){
+
+        // if their lengths are unequal
+        if(s1.length() != s2.length()){
+            return false;
+        }
+        Map<Character,Character> map = new HashMap<>();
+        for(int i=0;i<s1.length();i++){
+            if(map.containsKey(s1.charAt(i))){
+                return false;
+            }
+            map.put(s1.charAt(i),s2.charAt(i));
+
+        }
+
+        return true;
+
+    }
+}
